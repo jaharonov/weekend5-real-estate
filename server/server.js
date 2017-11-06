fac87466140f7e583e26e5d1d3c72b6e8c9996b8
Jaffa Aharonov <jaharonov@gmail.com>
2017-11-06 15:54:03 -0600

    mlab stuff added

--- server/server.js
+++ server/server.js
@@ -17,9 +17,22 @@ app.use('/listings', listingRoute);
 
 /** ---------- MONGOOSE ------------ **/
 var mongoose = require('mongoose');
-// gamestop is the name of our database
+
 // 27017 is the default mongo port number
-var databaseUrl = 'mongodb://localhost:27017/realestate';
+var mongoURI = 'mongodb://jaffa:mountainlion$$@ds249565.mlab.com:49565/jaffa';
+// process.env.MONGODB_URI will only be defined if you
+    // are running on Heroku
+    if(process.env.MONGODB_URI != undefined) {
+        +    // use the string value of the environment variable
+            +    mongoURI = process.env.MONGODB_URI;
+        } else {
+        // use the local database server
+           mongoURI = 'mongodb://localhost:27017/realestate';
+    }
+
+    mongoose.connect(databaseUrl, {
+   useMongoClient: true
+        });
 
 mongoose.connection.on('connected', function () {
     console.log('mongoose is connected!');
