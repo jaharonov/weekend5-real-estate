a57b4a75c9db50a79ba66e3d65cddeecdf0acc3f
Jaffa Aharonov <jaharonov@gmail.com>
2017-11-06 15:59:41 -0600

    trying to fix mlab connection

--- server/server.js
+++ server/server.js
@@ -23,8 +23,8 @@ var mongoURI = 'mongodb://jaffa:mountainlion$$@ds249565.mlab.com:49565/jaffa';
 // process.env.MONGODB_URI will only be defined if you
     // are running on Heroku
     if(process.env.MONGODB_URI != undefined) {
-        +    // use the string value of the environment variable
-            +    mongoURI = process.env.MONGODB_URI;
+            // use the string value of the environment variable
+                mongoURI = process.env.MONGODB_URI;
         } else {
         // use the local database server
            mongoURI = 'mongodb://localhost:27017/realestate';
